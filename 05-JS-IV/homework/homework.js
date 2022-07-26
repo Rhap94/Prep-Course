0a57a6446132fe0c793fd09ebde86e87c689d983
Rhap94 <murodaddy@gmail.com>
2022-07-26 01:11:07 -0500

    homework finally done, yeah

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -1,11 +1,21 @@
 // No cambies los nombres de las funciones.
 
+const TemplatePath = require("@11ty/eleventy/src/TemplatePath");
+
 function crearGato (nombre, edad) {
   // Crear un nuevo objeto con la propiedad "nombre" y el valor definido como el argumento "nombre".
   // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var obj = {
+    nombre: nombre,
+    edad: edad,
+    meow: function meow() {
+      return 'Meow!'
+    }
+  }
+    return obj;
 }
 
 function agregarPropiedad (objeto, property) {
@@ -13,6 +23,8 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto[property] = null;
+  return objeto;
 }
 
 function invocarMetodo (objeto, metodo) {
@@ -20,12 +32,16 @@ function invocarMetodo (objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  objeto[metodo]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
+  // Lo puedo hacer con dot notation o bracket notation ya que el valor de la propiedad es conocida 'numeroMisterioso'
   // Tu código:
+  var total = objetoMisterioso['numeroMisterioso'] * 5;
+  return total;
 
 }
 
@@ -33,40 +49,73 @@ function eliminarPropiedad (objeto, unaPropiedad) {
   // Elimina la propiedad de objeto cuyo nombre está pasado por el parametro unaPropiedad
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
+  //en esta tarea no puedo usar el dot notation debido ya que el valor de la propiedad no es visible, si o si
+  // se  tiene que usar el bracket notation
   // Tu código:
+  delete objeto[unaPropiedad];
+  return objeto;
 }
 
 function nuevoUsuario (nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+  var objeto = {
+    nombre: nombre,
+    email: email,
+    password: password
+  }
+    return objeto;
 }
 
 function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
+  // aca tambien puedo usar el bracket o dot ya que tengo un valor en condicion if() por definirse y  visible para la propiedad y es 'email'
   // Tu código:
+  if (usuario['email']) {
+    return true;
+  }
+    else {
+      return false;
+    }
+    
 }
 
 function tienePropiedad (objeto, propiedad) {
   // Devuelve "true" si el objeto (parámetro "objeto") tiene una propiedad (key) cuyo nombre es igual al valor del argumento "propiedad"
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
+  // puedo utilizar la otra solucion// return objeto.hasOwnProperty(propiedad)
   // Tu código:
+ if (objeto[propiedad]) {
+  return true;
+ }
+  return false;
+    
 }
 
 function verificarPassword (usuario, password) {
   // Comprueba si la "password" enviada coincide con la propiedad "password" del objeto "usuario"
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
+  /// Al tener el valor definido de la propiedad que es 'password' tenemos la opcion de usar bracket o dot notation
+  //En cambio si no habria valor definido para la propiedad solo podriamos usar el bracket notation
   // Tu código:
+  for (clave in usuario) {
+    if (usuario['password'] === password) {
+      return true;
+    }
+  }
+    return false;
 }
 
 function actualizarPassword (usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password = nuevaPassword;
+  return usuario;
 }
 
 function agregarAmigo (usuario, nuevoAmigo) {
@@ -74,6 +123,9 @@ function agregarAmigo (usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // Tu código:
+  usuario.amigos.push(nuevoAmigo);
+  return usuario;
+  
 }
 
 function pasarUsuarioAPremium (usuarios) {
@@ -81,7 +133,14 @@ function pasarUsuarioAPremium (usuarios) {
   // Cada objeto "usuario" tiene la propiedad "esPremium"
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
+  // Puedo invocar a la propiedad del objeto (esPremium) ya sea con dot notation o con bracket notation debido 
+  // a que es una propiedad  definida
   // Tu código:
+  for (var i = 0 ; i < usuarios.length ; i++) {
+    usuarios[i]['esPremium'] = true;
+  }
+    return usuarios;
+  
 }
 
 function sumarLikesDeUsuario (usuario) {
@@ -90,11 +149,22 @@ function sumarLikesDeUsuario (usuario) {
   // Cada objeto "post" tiene una propiedad llamada "likes" que es un entero (int/integer)
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
+  //Tambien puedo invocar a la propiedad del objeto (likes) ya sea con dot notation o con bracket notation debido 
+  //a que es una propiedad  definida
+  // inclusive en la formula for (var i = 0 ; i < usuario['posts'].length ; i++) puede ir con bracket notation o dot notation
+  //(var i = 0 ; i < usuario.posts.length ; i++) porque son propiedades con nombre y apellido
   // Tu código:
+  
+  var sumar = 0
+  for (var i = 0 ; i < usuario['posts'].length ; i++) {
+    sumar = sumar + usuario.posts[i]['likes'];
+  }
+    return sumar;
 }
+    
 
 function agregarMetodoCalculoDescuento (producto) {
-  // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
+  // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento" (puede ir con bracket notation o dot notation porque esta definido)
   // Este método debe multiplicar el "precio" del "producto" ("producto.precio" o "producto[precio]") y "porcentajeDeDescuento" para obtener el descuento
   // El método resta el descuento del precio y devuelve el precio con descuento
   // Devuelve el objeto "producto" al final de la función
@@ -103,7 +173,10 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto['calcularPrecioDescuento'] = function() {
+    return this.precio - (this.precio * this.porcentajeDeDescuento);
+  }
+    return producto;
 }
 
 // No modificar nada debajo de esta línea
